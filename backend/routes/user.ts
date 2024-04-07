f2a9110dd4e898900ba6b7b43a5af9b4f97d1f9e
unknown <vinayak20injamure@gmail.com>
2024-04-07 13:41:53 +0530

    commit

--- backend/routes/user.ts
+++ backend/routes/user.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { sign } from "hono/jwt";
-import {signupInput} from '@vinay100xdev/blog-common';
+
+
 export const userRouter = new Hono<{
     Bindings: {
         DATABASE_URL: string;
@@ -12,11 +13,8 @@ export const userRouter = new Hono<{
 
 userRouter.post('/signup', async (c) => {
     const body = await c.req.json();
-    const accees=signupInput.safeParse(body);
-    if(!accees){
-        c.status(411)
-        return c.json(" input invalid ")
-    }
+        
+    
     
     const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
